7f4984900433ee7d22a626dfd0c47396330bb8ff
Tommy Poll <tommy@poolle.co.uk>
2019-10-10 12:47:27 +0100

    Add extra features and output as markdown

--- src/main/java/EmailFetcher.java
+++ src/main/java/EmailFetcher.java
@@ -1,7 +1,3 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 import static java.lang.System.exit;
 import static java.lang.System.out;
 import static java.lang.System.in;
@@ -9,22 +5,21 @@ import static java.lang.System.in;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class EmailFetcher {
     public static void main(String[] args) {
         String ID = readID();
-        String URL = genURL(ID);
-        String name = parsePage(URL);
-        boolean inSoton = name != null;
-        if(!inSoton) {
-            name = ID;
+        Person person = new Person(ID);
+        out.println("# " + person.name); out.println();
+        if(person.inSoton) {
+            out.println("* " + person.email); out.println();
+            out.println("**Related people**: " + person.webpage + "/related_people"); out.println();
+        } else {
+            out.println("* " + person.webpage); out.println();
         }
-        out.println("# " + name); out.println();
-
-        //out.println(inSoton ? name : "This person doesn't exist or doesn't have a page.");
+        if(person.description != null) {
+            out.println(person.description); out.println();
+        }
     }
 
     private static String readID() {
@@ -38,35 +33,5 @@ public class EmailFetcher {
             exit(1);
         }
         return id;
-    }
-
-    private static String genURL(String username) {
-        String userID = username.split("@")[0];
-        String encodedID = null;
-        try {
-            encodedID = URLEncoder.encode(userID, StandardCharsets.UTF_8.toString());
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            exit(1);
-        }
-        return "https://www.ecs.soton.ac.uk/people/".concat(encodedID);
-    }
-
-    private static String parsePage(String URL) {
-        String name;
-        Document page = null;
-        try {
-            page = Jsoup.connect(URL).get();
-        } catch (IOException e) {
-            e.printStackTrace();
-            exit(1);
-        }
-        Elements nameElements = page.getElementsByAttributeValue("property", "name");
-        if(nameElements.size() < 1) {
-            name = null;
-        } else {
-            name = nameElements.first().text();
-        }
-        return name;
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,38 +1,60 @@
-import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 import static java.lang.System.exit;
 
-public class Person {
+class Person {
 
-    private String username;
-    private String name;
-    private String email;
-    private String webpage;
-    private String description;
-    private boolean inSoton;
+    String name;
+    String email;
+    String webpage;
+    String description;
+    boolean inSoton;
 
-    private Person() {}
-
-    public static String getNameFromUsername(String username) {
+    Person(String protoname) {
         String encodedID = null;
         try {
-            encodedID = URLEncoder.encode(username, StandardCharsets.UTF_8.toString());
+            encodedID = URLEncoder.encode(protoname, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
             exit(1);
         }
+        // Try Southampton first
         String URL = "https://www.ecs.soton.ac.uk/people/".concat(encodedID);
-
+        Document sotonPage;
+        try {
+            sotonPage = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            sotonPage = null;
+            e.printStackTrace();
+            exit(1);
+        }
+        if(sotonPage.getElementsByAttributeValue("property", "name").size() == 1) { // They are in Southampton
+            this.parseSotonPage(sotonPage);
+            this.setWebpage(URL);
+        } else {
+            // Google search
+            Document searchResults;
+            try {
+                searchResults = Jsoup.connect("https://www.google.co.uk/search").data("q", encodedID).get();
+            } catch (IOException e) {
+                searchResults = null;
+                e.printStackTrace();
+                exit(1);
+            }
+            this.setName(protoname);
+            this.setInSoton(false);
+            this.parseSearchResults(searchResults);
+        }
     }
 
-    public static Person parseSotonPage(@NotNull Document page) {
-        String
+    private void parseSotonPage(Document page) {
         // Get name
         Elements nameElements = page.getElementsByAttributeValue("property", "name");
         String name;
@@ -44,31 +66,65 @@ public class Person {
 
         // Get email address
         Elements emailElements = page.getElementsByAttributeValue("property", "email");
+        String email;
+        if(emailElements.size() < 1) {
+            email = null;
+        } else {
+            email = emailElements.first().text();
+        }
 
-        return new Person().setName(name);
+        // Get description
+        Elements descElements = page.getElementsByAttributeValue("property", "description");
+        if(descElements.size() == 1) {
+            this.setDescription(descElements.first().previousElementSibling().text()); // The real description is the element above this one
+        }
+
+        this.setName(name);
+        this.setEmail(email);
+        this.setInSoton(true);
     }
 
-    public Person setName(String newName) {
+    private void parseSearchResults(Document page) {
+        Elements descriptionElements = page.getElementsByClass("kno-rdesc");
+        if(descriptionElements.size() == 1) {
+            this.setDescription(descriptionElements.first().child(0).child(1).text());
+        }
+        Elements srgClassElements = page.getElementsByClass("srg");
+        if(srgClassElements.size() > 0) {
+            String firstLink = srgClassElements.first().getElementsByTag("a").first().attr("href");
+            Document sotonPage;
+            try {
+                sotonPage = Jsoup.connect(firstLink).get();
+            } catch (IOException e) {
+                sotonPage = null;
+                e.printStackTrace();
+                exit(1);
+            }
+            if(sotonPage.getElementsByAttributeValue("property", "name").size() == 1) { // They are in Southampton
+                this.parseSotonPage(sotonPage);
+                this.setWebpage(firstLink);
+                this.setInSoton(true);
+            }
+        }
+    }
+
+    private void setName(String newName) {
         this.name = newName;
-        return this;
     }
 
-    public Person setEmail(String newEmail) {
+    private void setEmail(String newEmail) {
         this.email = newEmail;
-        return this;
     }
 
-    public Person setWebpage(String newWebpage) {
+    private void setWebpage(String newWebpage) {
         this.webpage = newWebpage;
-        return this;
-    }
-
-    public Person setUsername(String newUsername) {
-        this.username = newUsername;
-        return this;
     }
 
     private void setDescription(String newDescription) {
         this.description = newDescription;
+    }
+
+    private void setInSoton(boolean inSoton) {
+        this.inSoton = inSoton;
     }
 }
